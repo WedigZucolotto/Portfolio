e23e8f06bcd3f2aae9a7e20dd7767d3acacce063
Douglas Zucolotto Gonçalves <douglaszuco2@gmail.com>
2024-02-24 23:52:18 -0300

    first-home-section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,40 @@
 import * as S from "./style";
+import Envelope from "../../img/envelope.svg";
 
 export const Home = () => {
   return (
-      <S.Home>
-        &lt;h1&gt;
-        &lt;/h1&gt;
-      </S.Home>
+    <S.Home>
+      <section className="left-section">
+        <span className="rendered-tag first-h1">&lt;h1&gt;</span>
+        <h1>
+          We <span className="blue-words">Program</span> Your
+          <br />
+          Feelings,
+          <span className="blue-words"> Code</span>
+          <br />
+          Emotions
+          <span className="rendered-tag second-h1">&lt;/h1&gt;</span>
+        </h1>
+
+        <span className="rendered-tag first-p">&lt;p&gt;</span>
+        <p className="paragraph">
+          We, a talented group with the latest technologies, can help you make{" "}
+          <br />
+          your dream come true.
+        </p>
+        <span className="rendered-tag second-p">&lt;/p&gt;</span>
+
+        <div className="lets-envolope">
+          <h2 className="lets-talk">Let's Talk</h2>
+          <img src={Envelope} alt="Envolope" className="envelope" />
+        </div>
+
+        <p className="var">
+          <span className="const">const</span>{" "}
+          <span className="github">githubLink</span> =
+          <span className="link"> “https://github.com/WedigZucolotto”</span>
+        </p>
+      </section>
+    </S.Home>
   );
 };
--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -1,5 +1,102 @@
 import styled from "styled-components";
 
 export const Home = styled.section`
-  background-color: red;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100vh;
+
+  .left-section {
+    color: white;
+    margin-left: 400px;
+    margin-bottom: 300px;
+    font-size: 60px;
+  }
+
+  .left-section > h1 {
+    font-family: "Ubuntu", sans-serif;
+    font-weight: 300;
+  }
+
+  .blue-words {
+    color: #12f7d6;
+  }
+
+  .rendered-tag {
+    color: #98faec;
+    font-size: 14px;
+    font-family: "IBM Plex Mono", monospace;
+  }
+
+  .first-h1 {
+    margin-left: -18px;
+    position: relative;
+    top: 10px;
+  }
+
+  .second-h1 {
+    position: relative;
+    left: 14px;
+  }
+
+  .first-p {
+    position: relative;
+    top: -35px;
+    left: -15px;
+  }
+
+  .second-p {
+    position: relative;
+    top: -60px;
+    left: -15px;
+  }
+
+  .paragraph {
+    font-family: "IBM Plex Mono", monospace;
+    font-size: 14px;
+    position: relative;
+    top: -33px;
+    left: 7px;
+  }
+
+  .lets-envolope {
+    display: flex;
+    align-items: center;
+    margin-top: -50px;
+    margin-left: 10px;
+  }
+
+  .lets-talk {
+    font-family: "IBM Plex Mono", monospace;
+    font-size: 32px;
+    font-weight: 500;
+    color: #12f7d6;
+  }
+
+  .envelope {
+    position: relative;
+    left: 25px;
+  }
+
+  .var {
+    font-size: 16px;
+    font-family: "Fira Code", monospace;
+    margin-top: 20px;
+  }
+
+  .const {
+    color: #4d5bce;
+  }
+
+  .github {
+    color: #43d9ad;
+  }
+
+  .link {
+    color: #e99287;
+  }
+
+  .right-section {
+    margin-right: 500px;
+  }
 `;
